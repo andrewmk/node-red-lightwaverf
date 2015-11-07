2eab073060b309fe387ecc5b67965cc28ffc690f
Andrew Hedges <andrew@hedges.me.uk>
2015-11-07 17:36:45 +0000

    Pass parameters into node to specify room, device etc. Also close sockets on node being removed.

--- lightwaverf.js
+++ lightwaverf.js
@@ -8,23 +8,77 @@ module.exports = function(RED) {
        	this.wifilink = RED.nodes.getNode(config.ip);
        	var lw = new lwrf({ip:this.wifilink.ip});
 
+        node.on('close', function() {
+                lw.sendSocket.close();
+                lw.receiveSocket.close();
+
+        });
+
         this.on('input', function(msg) {
         	var node = this;
 
         	var msg;
         	var payload = "empty";
 
-        	if (config.action=="on"){
-        		this.log("turning the device "+ config.device+" on, in room "+config.room);
-        		lw.turnDeviceOn(config.room, config.device, function(error, content) {node.log(error); node.log(content);});
-        	} else if (config.action=="off"){
-        		this.log("turning the device "+ config.device+" off, in room "+config.room);
-        		lw.turnDeviceOff(config.room, config.device, function(error, content) {node.log(error); node.log(content);});
-        	} else if (config.action=="dim"){
-        		this.log("dimming the device");
-        		lw.setDeviceDim(config.room, config.device, config.dim, function(error, content) {node.log(error); node.log(content);});
+                this.log("Room " + msg.room);
+                this.log("Device " + msg.device);
+                this.log("Action " + msg.action);
+                this.log("Dim " + msg.dim);
+
+                if (msg.room) {
+                        room = msg.room;
+                } else {
+                        room = config.room;
+                }
+
+                if (msg.device) {
+                        device = msg.device;
+                } else {
+                        room = config.device;
+                }
+
+                if (msg.action) {
+                        action = msg.action;
+                } else {
+                        action = config.action;
+                }
+
+                if (msg.dim) {
+                        dim = msg.dim;
+                } else {
+                        dim = config.dim;
+                }
+
+        	if (action=="on"){
+        		this.log("turning the device " + device + " on, in room " + room);
+        		lw.turnDeviceOn(room, device, function(error, content) {
+                                if (error) {
+                                        node.log(error);
+                                } else {
+                                        node.log(content);
+                                }
+                        });
+        	} else if (action=="off"){
+        		this.log("turning the device " + device + " off, in room " + room);
+        		lw.turnDeviceOff(room, device, function(error, content) {
+                                if (error) {
+                                        node.log(error);
+                                } else {
+                                        node.log(content);
+                                }
+                        });
+        	} else if (action=="dim"){
+        		this.log("dimming the device " + device + " to " + dim + " in room " + room);
+        		lw.setDeviceDim(room, device, dim, function(error, content) {
+                                if (error) {
+                                        node.log(error);
+                                } else {
+                                        node.log(content);
+                                }
+                        });
         	} 
 
+		node.send(msg);
         });
     }
     RED.nodes.registerType("lightwaverf",LightwaveRF);
